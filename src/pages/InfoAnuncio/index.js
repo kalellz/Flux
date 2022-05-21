95d831faf42f643b51d0d1830fe2a2aeeb94bc59
Victor <menezes.victorsantos@outlook.com>
2022-05-21 17:37:27 -0300

    new search bar design

--- src/pages/InfoAnuncio/index.js
+++ src/pages/InfoAnuncio/index.js
@@ -20,14 +20,18 @@ export default function Index() {
             <div class="logo">
               <h3 class="logo-flux">FLUX</h3>
             </div>
-            <div class="busca">
-              <input
-                type="text"
-                placeholder="Pesquise por qualquer coisa que quiser!"
-                class="pesquisa"
-              ></input>
-              <img src={buscar} width="24px"></img>
-            </div>
+            {/*new search bar code (start) */} 
+          <div class="group">
+            <svg class="icon" aria-hidden="true" viewBox="0 0 24 24">
+              <g>
+                <path d="M21.53 20.47l-3.66-3.66C19.195 15.24 20 13.214 20 11c0-4.97-4.03-9-9-9s-9 4.03-9 9 4.03 9 9 9c2.215 0 4.24-.804 5.808-2.13l3.66 3.66c.147.146.34.22.53.22s.385-.073.53-.22c.295-.293.295-.767.002-1.06zM3.5 11c0-4.135 3.365-7.5 7.5-7.5s7.5 3.365 7.5 7.5-3.365 7.5-7.5 7.5-7.5-3.365-7.5-7.5z">
+                </path>
+              </g>
+            </svg>
+            <input placeholder="Search" type="search" class="input">
+            </input>
+          </div>
+          {/*new search bar code (end) */}
             <div class="conta">
               <a href="" class="botao1">
                 Sair
