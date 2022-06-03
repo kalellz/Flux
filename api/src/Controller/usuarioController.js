a92e4d9cd69371f5a23e889fd9340003faa47483
Kalel <ZoroPlay1220@gmail.com>
2022-06-03 00:18:43 -0300

    correção login api

--- api/src/Controller/usuarioController.js
+++ api/src/Controller/usuarioController.js
@@ -1,4 +1,4 @@
-import { cadastro } from "../Repository/usuarioRepository.js";
+import { cadastro, login } from "../Repository/usuarioRepository.js";
 import { Router } from "express";
 const server = Router();
 
@@ -6,12 +6,28 @@ server.post("/usuario/cadastro", async (req, resp) => {
 	try {
 		const { nome, email, senha } = req.body;
 		const resposta = await cadastro(nome, email, senha);
-		resp.send(resposta);
+		if(!resposta) throw new Error('não foi possivel se cadastrar')
+		resp.send('cadastrado com sucesso');
 	} catch (err) {
 		resp.status(400).send({
 			erro: err.message,
 		});
 	}
 });
+server.get("/usuario/login", async (req,resp) => {
+	try{
+		const {email,senha} = req.body;
+		const resposta = await login(email,senha);
+		if(!email) throw new Error('email é obrigatorio')
+		if(!senha) throw new Error('senha é obrigatorio!')
+		if(!resposta || resposta == null) throw new Error("usuario não encontrado")
+		else
+			resp.send(resposta);
+	} catch (err) {
+		resp.status(400).send({
+			erro: err.message
+		});
+	}
+})
 
 export default server;
