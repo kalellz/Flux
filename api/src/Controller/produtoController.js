292713cdf332c535f2458c8fb9112cf255d7d88e
Kalel <ZoroPlay1220@gmail.com>
2022-06-02 22:58:38 -0300

    update in "alterar produtos"

--- api/src/Controller/produtoController.js
+++ api/src/Controller/produtoController.js
@@ -1,4 +1,4 @@
-import { alterarImagem, inserirProduto } from '../repository/produtoRepository.js'
+import { alterarImagem, alterarProduto, consultarProdutos, inserirProduto } from '../repository/produtoRepository.js'
 import multer from 'multer';
 import { Router } from 'express'
 
@@ -10,6 +10,22 @@ server.post('/produto', async (req, resp) => {
     try{
         const novoproduto = req.body
         const produtoinserido = inserirProduto(novoproduto);
+		if(!novoproduto.usuario)
+			throw new Error('Usuario é obrigatorio!')
+		if(!novoproduto.nome)
+			throw new Error('Nome do produto é obrigatorio!')
+		if(!novoproduto.categoria)
+			throw new Error('Categoria do produto é obrigatorio!')
+		if(!novoproduto.descricao)
+			throw new Error('Descrição do produto é obrigatorio!')
+		if(!novoproduto.preco)
+			throw new Error('Preço do produto é obrigatorio!')
+		if(!novoproduto.telefone)
+			throw new Error('Telefone é obrigatorio!')
+		if(!novoproduto.email)
+			throw new Error('E-mail é obrigatorio!')
+		if(!novoproduto.cep)
+			throw new Error('Cep é obrigatorio!')
         resp.send(produtoinserido)
     } catch(err){
         resp.status(400).send({
@@ -22,15 +38,54 @@ server.put('/produto/:id/capa', upload.single('capa'), async (req,resp) => {
     try{
         const { id } = req.params;
         const imagem = req.file.path;
-        
         const resposta = await alterarImagem(imagem, id)
         if(resposta != 1) throw new Error('A imagem não pode ser salva.')
-        resp.statu(204).send();
+		else
+        resp.status(204).send();
     } catch(err){
         resp.status(400).send({
             erro: err.message
         })
     }
+})
+server.put('/produto/:id', async (req,resp) => {
+	try{
+		const { id } = req.params;
+		const filme = req.body
+		const resposta = await alterarProduto(id,filme);
+		if(!novoproduto.usuario)
+			throw new Error('Usuario é obrigatorio!')
+		if(!novoproduto.nome)
+			throw new Error('Nome do produto é obrigatorio!')
+		if(!novoproduto.categoria)
+			throw new Error('Categoria do produto é obrigatorio!')
+		if(!novoproduto.descricao)
+			throw new Error('Descrição do produto é obrigatorio!')
+		if(!novoproduto.preco)
+			throw new Error('Preço do produto é obrigatorio!')
+		if(!novoproduto.telefone)
+			throw new Error('Telefone é obrigatorio!')
+		if(!novoproduto.email)
+			throw new Error('E-mail é obrigatorio!')
+		if(!novoproduto.cep)
+			throw new Error('Cep é obrigatorio!')
+		if (resposta != 1) throw new Error('produto nao pode ser alterado') 
+		else resp.status(204).send()
+	} catch(err){
+        resp.status(400).send({
+            erro: err.message
+        })}
+
+	})
+server.get('/produto', async (req,resp) => {
+	try{
+		const resposta = await consultarProdutos();
+		resp.send(resposta);
+	} catch(err){
+        resp.status(400).send({
+            erro: err.message
+        })
+	}
 })
 
 export default server;
--- api/src/Repository/produtoRepository.js
+++ api/src/Repository/produtoRepository.js
@@ -16,3 +16,26 @@ export async function alterarImagem(imagem, id){
     const [linhas] = await con.query(comando, [imagem, id])
     return linhas.affectedRows;
 }
+export async function alterarProduto(id, produto){
+	const comando = `
+	UPDATE tb_anuncio 
+   SET nm_produto    = ?,
+       ds_produto    = ?,
+       dc_preco      = ?,
+       ds_telefone   = ?,
+       ds_email      = ?,
+       ds_cep        = ?
+ WHERE id_anuncio = ?`
+	const [linhas] = await con.query(comando, [[produto.usuario, produto.nome, produto.categoria, produto.descricao, produto.imagem, produto.preco, produto.telefone, produto.email, produto.cep]])
+}
+export async function consultarProdutos() {
+	const comando = `
+		SELECT id_anuncio			    id,
+			nm_produto             		nome,
+			ds_produto        			descricao,
+			dc_preco              		preco,
+			img_produto           		imagem
+		FROM tb_anuncio`
+	const [linhas] = await con.query(comando);
+	return linhas;
+}
