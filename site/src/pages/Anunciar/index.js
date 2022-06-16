4c6e00b905344e4c039495a6cf684d1243e28e66
kalel <ZoroPlay1220@gmail.com>
2022-06-16 17:59:49 -0300

    update bobo foi errado

--- site/src/pages/Anunciar/index.js
+++ site/src/pages/Anunciar/index.js
@@ -193,7 +193,7 @@ export default function Index() {
           </div>
         </div>
         <div className="ultrapassado">
-          <h1 className="titulo-ultra">Fotos do produto</h1>
+          <h1 className="titulo-ultra">Foto do produto</h1>
           <form>
             <div className="div-escolherimagem" ocClick={escolherImagem}>
               <label className="main_div"  for="arquivo">
